a699a995a0e94b944762069879f01a7364e8bae7
Ashiq-jr <ashiqjr7@gmail.com>
2023-10-15 23:07:21 +0530

    Working / Tested

---
+++ Controllers/ValidatorController.cs
@@ -0,0 +1,28 @@
+﻿using Microsoft.AspNetCore.Mvc;
+
+namespace sample3.Controllers
+{
+    public class ValidatorController : Controller
+    {
+        public IActionResult Validator()
+        {
+            if (User.Identity!.IsAuthenticated)
+            {
+                return RedirectToAction("Privacy", "Home");
+            }
+
+            return RedirectToAction("Login", "Validator");
+        }
+
+        public IActionResult Login() { 
+
+            return View();
+        
+        }
+
+        public IActionResult Signout() {
+
+            return RedirectToAction("Login", "Validator");
+        }
+    }
+}
